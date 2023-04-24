3d9e4bd5d2d589ac8707ee705831dbd8b7b1fea3
Camila Colanjo Pereira <c.colanjo@gmail.com>
2023-04-24 20:59:26 -0300

    ajustes

---
+++ scheduleAppointment/schedule-appointment-service/Interface/ISendEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schedule_appointment_service.Interface
+{
+    public interface ISendEmailService
+    {
+
+        public Task<string> SendEmailAsync(string email, string text, string name, string apiKey);
+    }
+}
